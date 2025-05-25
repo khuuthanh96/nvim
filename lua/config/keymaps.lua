fb37bd78f952a0b2ec6c1cfd1a13c49f4ed2c1f2
Tyler <khuuthanh1001@gmail.com>
2025-05-25 14:04:47 +0900

    disable mini.indentscope, add keymap to tmuxsessionizer, config noice

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.keymap.set(
+  "n",
+  "<C-f>",
+  ":silent !tmux neww tmux-sessionizer<CR>",
+  { noremap = true, silent = true, desc = "Open tmux session selector" }
+)
